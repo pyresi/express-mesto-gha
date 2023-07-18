39cd005aa7e0362e00682bab837505e71715a0d2
pyresi <pyresi@yandex.ru>
2023-07-18 23:05:48 +0300

    Fix error constants

--- util/handleErrors.js
+++ util/handleErrors.js
@@ -1,9 +1,13 @@
+const MISSING_ERROR_CODE = 404;
+const INVALID_ERROR_CODE = 400;
+const DEFAULT_ERROR_CODE = 500;
+
 module.exports.handleErrors = (res, err) => {
   // console.log(err);
   if (err.name === 'ValidationError' || err.name === 'CastError') {
-    return res.status(400).send({ message: err.message });
+    return res.status(INVALID_ERROR_CODE).send({ message: err.message });
   } if (err.name === 'MissingError') {
-    return res.status(404).send({ message: err.message });
+    return res.status(MISSING_ERROR_CODE).send({ message: err.message });
   }
-  return res.status(500).send({ message: 'Ошибка. Что-то пошло не так.' });
+  return res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка. Что-то пошло не так.' });
 };
