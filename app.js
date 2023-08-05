d1961116d923a409a79093f10c1a5344e7df08a4
pyresi <pyresi@yandex.ru>
2023-08-06 01:15:31 +0300

    Fix joi email validation

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ app.listen(PORT, () => {
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required()
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
